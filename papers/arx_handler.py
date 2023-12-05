d7f698c2fec613f78deaf1f0bfa56ce21eb8216d
mirkan <mirkanbaba1@gmail.com>
2023-12-05 14:29:20 -0500

    pep errors and dunden calls cleaned

--- papers/arx_handler.py
+++ papers/arx_handler.py
@@ -18,34 +18,34 @@ def downloadPDF_create_text_arx(paper):
     title = paper.title
     authors = ', '.join([str(auth) for auth in paper.authors])
     summary = paper.summary
-    journal = ''
-    if paper.journal_ref:
-        journal = journal_ref
-
+    journal = paper.journal_ref or ''
     # Create the dir
-    n_dirs = [0 for path in os.listdir('./') if os.path.isdir(path)].__len__() + 1
+    n_dirs = len([0 for path in os.listdir('./') if os.path.isdir(path)]) + 1
     dir_name = "1_" + str(n_dirs)
     os.mkdir(dir_name)
 
-#   Download , save pdf
-    pdf_response = requests.get((paper.entry_id + '.pdf').replace('abs', 'pdf'))
+    # Download , save pdf
+    pdf_response = requests.get((paper.entry_id + '.pdf').replace('abs', 'pdf'), timeout=180)
     with open(os.path.join(dir_name, dir_name + '.pdf'), 'wb') as f:
         f.write(pdf_response.content)
 
-#   Create .md and edit and save
+    # Create .md and edit and save
     title = '# ' + title
     summary = '## Abstract\n' + summary
     publish_date = 'Publish date: ' + publish_date
     current_date = 'Data added to papers: ' + current_date
     authors = 'Authors: ' + authors
     journal = 'Journal: ' + journal
-    with open(os.path.join(dir_name, 'README.md'), 'a') as f:
+    with open(os.path.join(dir_name, 'README.md'), 'a', encoding='utf-8') as f:
         for info in [title, summary, authors, publish_date, current_date]:
             f.write(info + '\n\n')
-# Example usage
-def main():
-    parser = argparse.ArgumentParser(prog='xarviToMD')
-    parser.add_argument('-i', '--id') # Paper id
-    downloadPDF_create_text_arx(fetch_data_arx(parser.parse_args().id))
 
-main()
+if __name__ == '__main__':
+    def main():
+        """
+            Example usage
+        """
+        parser = argparse.ArgumentParser(prog='xarviToMD')
+        parser.add_argument('-i', '--id') # Paper id
+        downloadPDF_create_text_arx(fetch_data_arx(parser.parse_args().id))
+    main()
